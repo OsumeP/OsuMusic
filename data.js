2be4502a4e6f9ca2cdc5ed0059866db657833b50
Juan David Patarroyo Sanchez <juandavid.patarroyosanchez@gmail.com>
2021-08-26 18:11:12 -0500

    Start of the Music Program Code

---
+++ data.js
@@ -0,0 +1,32 @@
+let song = [
+    {
+        name: 'Black Catcher',
+        path: 'Assets/OsuMusics/Black_Catcher.mp3',
+        artist: 'VickeBlanka',
+        cover: 'Assets/Images/Hanako_OsuMusic.jpg'
+    },
+    {
+        name: 'Crystaline Chill',
+        path: 'Assets/OsuMusics/Crystaline_Chill.mp3',
+        artist: 'SquareEnix',
+        cover: 'Assets/Images/Uraraka_OsuMusic.jpg'
+    },
+    {
+        name: 'Memories',
+        path: 'Assets/OsuMusics/Memories.mp3',
+        artist: 'One Ok Rock',
+        cover: 'Assets/Images/Hyouka_OsuMusic.jpg'
+    },
+    {
+        name: 'Robot Love',
+        path: 'Assets/OsuMusics/Robot_Love.mp3',
+        artist: 'Klaypex ft.GRÉTA',
+        cover: 'Assets/Images/Waifu_OsuMusic.jpg'
+    },
+    {
+        name: 'Safe and Sound',
+        path: 'Assets/OsuMusics/Safe_and_Sounds',
+        artist: 'Different Haven',
+        cover: 'Assets/Images/Diona_OsuMusic.png'
+    }
+]
